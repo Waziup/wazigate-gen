992ec243df644c614d16dc375b957d4e71aff837
Corentin Dupont <corentin.dupont@gmail.com>
2026-01-21 17:16:54 +0100

    Update 00-run.sh

--- stage3/02-install-docker/00-run.sh
+++ stage3/02-install-docker/00-run.sh
@@ -54,6 +54,6 @@ install -m 644 files/daemon.json "$ROOTFS_DIR/etc/docker/daemon.json"
 
 # Install docker-compose
 
-curl -L "https://github.com/docker/compose/releases/download/v2.4.1/docker-compose-linux-aarch64" -o $ROOTFS_DIR/usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v5.0.2/docker-compose-linux-aarch64" -o $ROOTFS_DIR/usr/local/bin/docker-compose
 
 chmod +x "$ROOTFS_DIR/usr/local/bin/docker-compose"
