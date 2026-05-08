b6987ad578bcb4085e0317a2f4180226bfbf3a06
Felix Markwordt <24inchstreetbikesoldier@gmail.com>
2026-05-08 16:16:31 +0200

    mongo fix

--- stage3/01-install-packages/00-run.sh
+++ stage3/01-install-packages/00-run.sh
@@ -2,8 +2,7 @@
 
 # add for mongodb source location
 on_chroot <<EOF 
-wget -qO- https://www.mongodb.org/static/pgp/server-4.4.asc | gpg --dearmor > /usr/share/keyrings/mongodb-archive-keyring.gpg
-
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
 
 echo \
 "deb [ arch=arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | tee "/etc/apt/sources.list.d/mongodb-org-4.4.list"
